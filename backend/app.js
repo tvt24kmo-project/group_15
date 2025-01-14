322e67c28285d2be516442887ea5f4a5485efdc2
mintusmaximus <156218528+mintusmaximus@users.noreply.github.com>
2025-01-14 17:27:58 +0200

    mwb tiedoston korjaus, nodemon, mysql asennus node.js:ään, envtemplate tehty, tietokannan yhteydet tehty ja testattu, get all customers toimii, get by fname toimii

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/customers');
 
 var app = express();
 
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// MUISTA LISÄTÄ ROUTERIT JA NIIDEN POLUT TÄNNE ETTÄ EI TULE 404 VIRHETTÄ
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/customers', usersRouter);
 
 module.exports = app;
