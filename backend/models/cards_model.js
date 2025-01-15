f45429d3c672a9de02d18e5a063da8e47e906cc7
mintusmaximus <156218528+mintusmaximus@users.noreply.github.com>
2025-01-16 00:33:24 +0200

    pinhash fix

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -11,18 +11,18 @@ const cards = {
     },
 
 
-//kortin salasanan kryptaus
+    //kortin salasanan kryptaus
     add: function (cards_data, callback) {
-            bcrypt.hash(cards_data.pinhash, 10, function (err, hashed_pinhash) {
-                return db.query("INSERT INTO cards(cardtype, pinhash, islocked, credit_limit, cardnumber) values(?,?,?,?,?)",
+        bcrypt.hash(cards_data.pinhash, 10, function (err, hashed_pinhash) {
+            return db.query("INSERT INTO cards(cardtype, pinhash, islocked, credit_limit, cardnumber) values(?,?,?,?,?)",
                 [cards_data.cardtype, hashed_pinhash, cards_data.islocked, cards_data.credit_limit,
-                 cards_data.cardnumber], callback);
-            })
-        },
+                cards_data.cardnumber], callback);
+        })
+    },
     update: function (id, cards_data, callback) {
         bcrypt.hash(cards_data.pinhash, 10, function (err, hashed_pinhash) {
-        return db.query("UPDATE cards SET cardtype=?, pinhash=?, islocked=?, credit_limit=? WHERE idcard=?",
-            [cards_data.cardtype, cards_data.pinhash, cards_data.islocked, cards_data.credit_limit, id], callback);
+            return db.query("UPDATE cards SET cardtype=?, pinhash=?, islocked=?, credit_limit=? WHERE idcard=?",
+                [cards_data.cardtype, hashed_pinhash, cards_data.islocked, cards_data.credit_limit, id], callback);
         })
     }
 }
