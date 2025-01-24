78fd478e43540247f4d91070aae4bbe3d867f660
mintusmaximus <156218528+mintusmaximus@users.noreply.github.com>
2025-01-24 23:35:57 +0200

    proseduureja eteenpäin, ei vielä toimi

--- backend/models/procedures_model.js
+++ backend/models/procedures_model.js
@@ -2,8 +2,12 @@ const db = require('../database');
 
 const procedures = {
     withdraw: function (account_id, amount, callback) {
-        return db.query("CALL withdraw(?, ?)", [account_id, amount], callback);
-    }
+        return db.query("CALL WithdrawFunds(?, ?)", [account_id, amount], callback);
+    },
+
+    getAccountType: function (account_id, callback) {
+        return db.query("CALL GetAccountType (?)", [account_id], callback);
+    },
 };
 
 module.exports = procedures;
--- backend/routes/procedures.js
+++ backend/routes/procedures.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const procedures = require('../models/procedures_model');
 
-
-
 router.post('/withdraw', function (req, res) {
     procedures.withdraw(req.body.account_id, req.body.amount, function (err, result) {
         if (err) {
@@ -13,5 +11,16 @@ router.post('/withdraw', function (req, res) {
         }
     });
 });
+
+router.post('/getAccountType', function (req, res) {
+    procedures.getAccountType(req.body.account_id, function (err, result) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
 
 module.exports = router;
