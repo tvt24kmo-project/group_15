3d029e8d958784f45bc7b038b96ca706bc25b8c6
mintusmaximus <156218528+mintusmaximus@users.noreply.github.com>
2025-01-29 20:20:08 +0200

    Vielä *save all* qt creatorista perille

--- bank-automat/withdrawcash.h
+++ bank-automat/withdrawcash.h
@@ -26,6 +26,8 @@ private slots:
 
     void on_button100_clicked();
 
+    void on_withdrawCash_clicked();
+
 private:
     int MINBILLSIZE = 20; // pienin seteli mitä automaatti antaa muun määrän nostoon (esim 2x20€ = ok, 1x5 tai 3x5 = ei ok)
     int cashAmount = 0; // muuttuja jossa arvo muuttuu näppäinten/muun määrän perusteella
