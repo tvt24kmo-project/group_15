6eec1c40200e8c2489df95603ea1805b12fb5a52
mintusmaximus <156218528+mintusmaximus@users.noreply.github.com>
2025-01-29 19:28:53 +0200

    muun määrän tarkistamiset tehty ja virheikkuna

--- bank-automat/withdrawcash.cpp
+++ bank-automat/withdrawcash.cpp
@@ -1,5 +1,6 @@
 #include "withdrawcash.h"
 #include "ui_withdrawcash.h"
+#include <QErrormessage>
 
 
 WithdrawCash::WithdrawCash(QWidget *parent)
@@ -17,25 +18,28 @@ WithdrawCash::~WithdrawCash()
 void WithdrawCash::on_buttonWithdrawOther_clicked()
 {
     QString Qamount = ui->lineEditCustomAmount->text(); // haetaan muun rahamäärän luku
-    qDebug()<<"Yritetään nostaa rahaa:" + Qamount + "€";
+    qDebug()<<"Yritetään nostaa rahaa: " + Qamount + "€";
 
     bool ok; // qt abstraction moment
     int amount = Qamount.toInt(&ok); // viedään intiksi ku qstring ei toimi if statementissä
 
-    if (!ok)
+    if (!ok) // jos intiksi konverttaaminen ei toimi (jos syötetään esim kirjaimia)
     {
         qDebug()<<"convert to int failed";
+        QErrorMessage *errorMessage = new QErrorMessage(this);
+        errorMessage->showMessage("Virheellinen arvo, syötä kokonaisluku kenttään");
     }
 
-    else
+    else // jos intiksi muunto menee läpi
     {
-        if (amount%MINBILLSIZE == 0) // jos
+        // jos jaollinen minimi setelillä (tällä hetkellä 20) tai 50 setelillä (pakosta tosi aina 50€+ summilla, joten or statement ei haittaa)
+        if (amount%MINBILLSIZE == 0 || amount%50 == 0)
         {
             qDebug()<<"Jaollinen " + QString::number(MINBILLSIZE) + ":llä(pienin seteli)"; // qstring moment
         }
         else
         {
-            qDebug()<<"Ei jaollinen 20:llä ";
+            qDebug()<<"Ei jaollinen" + QString::number(MINBILLSIZE) + ":llä (pienin seteli)";
         }
     }
 
--- bank-automat/withdrawcash.h
+++ bank-automat/withdrawcash.h
@@ -19,7 +19,7 @@ private slots:
     void on_buttonWithdrawOther_clicked();
 
 private:
-    int MINBILLSIZE = 20; // pienin seteli mitä automaatti antaa muun määrän nostoon (esim 2x20 = ok, 1x5 tai 3x5 = ei ok)
+    int MINBILLSIZE = 20; // pienin seteli mitä automaatti antaa muun määrän nostoon (esim 2x20€ = ok, 1x5 tai 3x5 = ei ok)
     Ui::WithdrawCash *ui;
 };
 
